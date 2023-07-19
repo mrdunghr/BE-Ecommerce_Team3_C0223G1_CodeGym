348f07ebc703f1c835c4de64b895f78957b521e7
Mr.Dunghr <mrdunghr@gmail.com>
2023-07-19 10:08:12 +0700

    chức năng đăng ký customer

---
+++ src/main/java/com/team3/ecommerce/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.team3.ecommerce.controller;
+
+import com.team3.ecommerce.entity.Customer;
+import com.team3.ecommerce.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
+        Optional<Customer> customer = customerService.getCustomerById(id);
+        if (customer.isPresent()) {
+            return ResponseEntity.ok(customer.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/check-email")
+    public ResponseEntity<Boolean> isEmailUnique(@RequestParam String email) {
+        boolean isUnique = customerService.isEmailUnique(email);
+        return ResponseEntity.ok(isUnique);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerCustomer(@RequestBody Customer customer) {
+        boolean isUnique = customerService.isEmailUnique(customer.getEmail());
+        if (!isUnique) {
+            return ResponseEntity.badRequest().body("Email already exists.");
+        }
+
+        customerService.registerCustomer(customer);
+        return ResponseEntity.ok("Registration successful.");
+    }
+}
---
+++ src/main/java/com/team3/ecommerce/service/CustomerService.java
@@ -0,0 +1,51 @@
+package com.team3.ecommerce.service;
+
+import com.team3.ecommerce.entity.AuthenticationType;
+import com.team3.ecommerce.entity.Country;
+import com.team3.ecommerce.entity.Customer;
+import com.team3.ecommerce.repository.CountryRepository;
+import com.team3.ecommerce.repository.CustomerRepository;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    private CountryRepository countryRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Optional<Customer> getCustomerById(Integer id) {
+        return customerRepository.findById(id);
+    }// lấy ra một customer
+    public List<Country> listAllCountries() {
+        return countryRepository.findAllByOrderByNameAsc();
+    } // danh sách quốc gia
+
+    public boolean isEmailUnique(String email) {
+        Customer customer = customerRepository.findByEmail(email);
+        return customer == null;
+    } // kiểm tra email là duy nhất
+
+    public void registerCustomer(Customer customer) {
+        customer.setEnabled(true);
+        customer.setCreatedTime(new Date());
+        customer.setAuthenticationType(AuthenticationType.DATABASE);
+        customer.setAddressLine1(" ");
+        customer.setAddressLine2(" ");
+        customer.setCity(" ");
+        customer.setPostalCode(" ");
+
+        String randomCode = RandomString.make(64);
+        customer.setVerificationCode(randomCode);
+
+        customerRepository.save(customer);
+    } // đăng kí tài khoản user customer mới
+}
