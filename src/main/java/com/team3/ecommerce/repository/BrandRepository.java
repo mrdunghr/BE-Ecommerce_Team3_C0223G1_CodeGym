ae1fdc5d9758c03b8961d2a2eabd98e8de468d61
Mr.Dunghr <mrdunghr@gmail.com>
2023-07-20 15:04:58 +0700

    reset default

--- src/main/java/com/team3/ecommerce/repository/BrandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team3.ecommerce.repository;
-
-import com.team3.ecommerce.entity.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrandRepository extends JpaRepository<Brand, Integer> {
-}
--- src/main/java/com/team3/ecommerce/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team3.ecommerce.repository;
-
-import com.team3.ecommerce.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
