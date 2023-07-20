5c17372366921199de82e66e69bf54af237da3ea
ritakool <jokernguyen1994@gmail.com>
2023-07-20 13:51:42 +0700

    tìm kiếm sản phẩm trong 1 shop: toàn bộ sản phẩm, tìm theo tên, tìm theo category, tìm theo brand

--- src/main/java/com/team3/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/team3/ecommerce/repository/ProductRepository.java
@@ -1,10 +1,29 @@
 package com.team3.ecommerce.repository;
 
+import com.team3.ecommerce.entity.Brand;
+import com.team3.ecommerce.entity.Category;
+import com.team3.ecommerce.entity.Shop;
 import com.team3.ecommerce.entity.product.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+    @Query("SELECT p FROM Product p WHERE p.shop = :shop AND" +
+            "(:keyword IS NULL OR (p.name LIKE %:keyword% OR p.alias LIKE %:keyword%)) AND " +
+            "(:category IS NULL OR p.category = :category) AND " +
+            "(:brand IS NULL OR p.brand = :brand)")
+    Page<Product> findProductByShopKeywordAndCategory(@Param("shop") Shop shop,
+                                                          @Param("keyword") String keyword,
+                                                          @Param("category") Category category,
+                                                          @Param("brand")Brand brand,Pageable pageable);
 }
---
+++ src/main/java/com/team3/ecommerce/service/BrandService.java
@@ -0,0 +1,19 @@
+package com.team3.ecommerce.service;
+
+import com.team3.ecommerce.entity.Brand;
+import com.team3.ecommerce.repository.BrandRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BrandService {
+    @Autowired
+    BrandRepository brandRepository;
+    public Optional<Brand> findBrandById(Integer id) {
+        return brandRepository.findById(id);
+    }
+}
