5c8fb8703f754cf97cd1521cf8273d5d3bf4cc18
ritakool <jokernguyen1994@gmail.com>
2023-07-24 11:32:36 +0700

    CrossOrigin và sửa lại api của hiển thị toàn bộ category

--- src/main/java/com/team3/ecommerce/controller/CategoryController.java
+++ src/main/java/com/team3/ecommerce/controller/CategoryController.java
@@ -4,10 +4,11 @@ import com.team3.ecommerce.entity.Category;
 import com.team3.ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/category")
 public class CategoryController {
@@ -15,7 +16,7 @@ public class CategoryController {
     CategoryService categoryService;
 
     // Lấy ra list category
-    @GetMapping("")
+    @GetMapping("/all")
     public ResponseEntity<Iterable<Category>> showAllCategory() {
         Iterable<Category> categoryIterable = categoryService.findAllCategory();
         return ResponseEntity.ok(categoryIterable);
