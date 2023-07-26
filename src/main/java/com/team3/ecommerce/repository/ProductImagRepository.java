e059ec4dea189237496497ca419830a152904ced
ritakool <jokernguyen1994@gmail.com>
2023-07-26 11:34:25 +0700

    Thêm nhiều ảnh cho 1 sản phẩm khi tạo mới

--- src/main/java/com/team3/ecommerce/repository/ProductImagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team3.ecommerce.repository;
-
-import com.team3.ecommerce.entity.product.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductImagRepository extends JpaRepository<ProductImage, Integer> {
-}
