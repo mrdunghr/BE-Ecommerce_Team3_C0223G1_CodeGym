4eda877295a19e23bd195d6c14c7b1c04487ba60
ritakool <jokernguyen1994@gmail.com>
2023-07-27 07:58:35 +0700

    sửa phương thức thêm sản phẩm vào giỏ hàng

--- src/main/java/com/team3/ecommerce/controller/CartItemController.java
+++ src/main/java/com/team3/ecommerce/controller/CartItemController.java
@@ -5,6 +5,7 @@ import com.team3.ecommerce.entity.Customer;
 import com.team3.ecommerce.entity.product.Product;
 import com.team3.ecommerce.service.CartItemService;
 import com.team3.ecommerce.service.CustomerService;
+import com.team3.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,8 @@ public class CartItemController {
     private CartItemService cartItemService;
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private ProductService productService;
     @GetMapping("/view")
     public ResponseEntity<List<CartItem>> viewCart(HttpSession session){
         Integer customerId = (Integer) session.getAttribute("customerId");
@@ -40,6 +43,9 @@ public class CartItemController {
         }
         // lấy danh sách các mặt hàng có trong giỏ hàng của khách hàng dựa vào customerId và productId
         List<CartItem> cartItems = cartItemService.cardItemRepository.findByCustomerId(customerId);
+        if (cartItem.getQuantity() == 0) {
+            cartItem.setQuantity(1);
+        }
         if (!cartItems.isEmpty()) {
             for (CartItem item: cartItems) {
                 if (item.getProduct().getId().equals(cartItem.getProduct().getId())) {
@@ -47,12 +53,13 @@ public class CartItemController {
                     item.setQuantity(item.getQuantity() + cartItem.getQuantity());
                     cartItemService.saveCartItem(item);
                     return ResponseEntity.ok(item);
-                } break;
+                }
             }
         }
         Customer customer = customerService.getCustomerById(customerId).get();
         // mặt hàng chưa có thì thêm mới
         cartItem.setCustomer(customer);
+        cartItem.setProduct(productService.findById(cartItem.getProduct().getId()).get());
         CartItem newCartItem = cartItemService.saveCartItem(cartItem);
         return ResponseEntity.ok(newCartItem);
     }
