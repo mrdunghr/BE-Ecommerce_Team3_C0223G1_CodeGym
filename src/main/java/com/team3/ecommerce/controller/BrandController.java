ee4452a88b4634b5724e6830424556861ab5c263
Mr.Dunghr <mrdunghr@gmail.com>
2023-07-28 09:57:24 +0700

    chức năng sửa/cập nhập brand

--- src/main/java/com/team3/ecommerce/controller/BrandController.java
+++ src/main/java/com/team3/ecommerce/controller/BrandController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -73,5 +74,19 @@ public class BrandController {
         }
         brandService.deleteBrandsById(id);
         return ResponseEntity.ok("Brand deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Brand> updateBrand(@PathVariable Integer id, @RequestBody Brand updateBrand) {
+        Brand existingBrand = brandService.findBrandById(id).get();
+        if (existingBrand == null) {
+            return ResponseEntity.notFound().build();
+        }
+        existingBrand.setName(updateBrand.getName());
+        existingBrand.setLogo(updateBrand.getLogo());
+        existingBrand.setCategories(updateBrand.getCategories());
+
+        updateBrand = brandService.save(existingBrand);
+        return new ResponseEntity<>(updateBrand, HttpStatus.OK);
     }
 }
