0d6f4e06bef1ef4e90b1038f0a1e1d03b8a0cad0
mariohieu09 <trantrunghieu20092001@gmail.com>
2023-08-01 21:30:05 +0700

    done thanh toan

--- src/main/java/com/team3/ecommerce/controller/hieu/customerOrderController.java
+++ src/main/java/com/team3/ecommerce/controller/hieu/customerOrderController.java
@@ -27,20 +27,40 @@ public class customerOrderController {
         return new ResponseEntity<>(orderDetailService.getListOrderDetailByCustomerId(customerId), HttpStatus.OK);
     }
 
-    @PutMapping("/confirm-order/{id}")
-    public ResponseEntity<?> confirmOrder(@PathVariable Integer id){
+    @PutMapping("/confirm-order/{action}/{id}")
+    public ResponseEntity<?> confirmOrder(@PathVariable String action ,@PathVariable Integer id){
         OrderDetail confirmOrder = orderDetailService.getOrderById(id);
         int order_quan = confirmOrder.getQuantity();
         int product_quan = confirmOrder.getProduct().getQuantity();
-        if(order_quan > product_quan){
-            return new ResponseEntity<>("The quantity is not enough!", HttpStatus.BAD_REQUEST);
-        }else {
-            confirmOrder.setStatus(OrderStatus.PROCESSING);
-            Product p = confirmOrder.getProduct();
-            p.setQuantity(product_quan - order_quan);
-            orderDetailService.saveOrder(confirmOrder);
-            productService.editProduct(p);
-            return new ResponseEntity<>(HttpStatus.OK);
+        switch (action) {
+            case "confirm":
+                if(order_quan > product_quan){
+                    return new ResponseEntity<>("The quantity is not enough!", HttpStatus.BAD_REQUEST);
+                }else {
+                    confirmOrder.setStatus(OrderStatus.PROCESSING);
+                    Product p = confirmOrder.getProduct();
+                    p.setQuantity(product_quan - order_quan);
+                    orderDetailService.saveOrder(confirmOrder);
+                    productService.editProduct(p);
+                    return new ResponseEntity<>(HttpStatus.OK);
+                }
+            case "paid":
+                confirmOrder.setStatus(OrderStatus.PAID);
+                orderDetailService.saveOrder(confirmOrder);
+                return new ResponseEntity<>(HttpStatus.OK);
+            case "return":
+                confirmOrder.setStatus(OrderStatus.RETURNED);
+                orderDetailService.saveOrder(confirmOrder);
+                Product p = confirmOrder.getProduct();
+                p.setQuantity(product_quan + order_quan);
+                productService.editProduct(p);
+                return new ResponseEntity<>(HttpStatus.OK);
+            case "cancel":
+                confirmOrder.setStatus(OrderStatus.CANCELLED);
+                orderDetailService.saveOrder(confirmOrder);
+                return new ResponseEntity<>(HttpStatus.OK);
+            default:
+                return ResponseEntity.ok("OK");
         }
     }
 
